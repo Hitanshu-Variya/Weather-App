9e4d44d3fa86f34e4520d55ca4137e9e13a93689
Hitanshu Variya <hjvariya1@gmail.com>
2024-06-08 19:18:16 +0530

    Dynamic Website without the Search Component Deployed

--- Index.js
+++ Index.js
@@ -1,17 +1,40 @@
-const path = require("Path");
+const path = require("path");
+const fetch = require("node-fetch");
+const hbs = require("hbs");
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-const StaticPath = path.join(__dirname, "Page Files");
-console.log(StaticPath);
-app.use(express.static(StaticPath));
+let weatherData = null;
+app.use(express.static(path.join(__dirname, '/Public')));
+
+const ViewsPath = path.join(__dirname, "/Templates/views");
+const PartialsPath = path.join(__dirname, "/Templates/Partials");
+
+app.set('view engine', '.hbs');
+app.set("views", ViewsPath);
+hbs.registerPartials(PartialsPath);
 
 app.get("/", (req, res) => {
-    res.send(" Success! ");
-    res.end();
+    res.render("Weather", {
+        data: weatherData
+    });
 }); 
+
+function CallExternalAPI() {
+    let URL = `https://api.openweathermap.org/data/2.5/weather?q=Surat&appid=d9605fab0676140197689aa9ff9523f2`;
+    fetch(URL)
+    .then((API_response) => {
+        return API_response.json()  
+    })
+    .then((json_response) => {
+        weatherData = json_response;
+    })
+    .catch((error) => console.log(error));
+}
 
 app.listen(PORT, () => {
     console.log(`Server Running on Port : ${PORT}`);
+    CallExternalAPI();
 })
